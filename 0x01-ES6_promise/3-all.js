9c0c9f2943716db6e84e0980ca75ed5c3389690b
Aanuoluwa101 <aanuayodeji101@gmail.com>
2023-08-06 17:58:33 -0700

    fixed 2, 3 and 9

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,10 +1,12 @@
 /* eslint-disable no-console */
-import { uploadPhoto, createUser } from "./utils";
+import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-    Promise.all([uploadPhoto(), createUser()]).then((data) => {
-        console.log(`${data[0].body} ${data[1].firstName} ${data[1].lastName}`)
-    }).catch(() => {
-        console.log('Signup system offline')
-    })
+  const photo = uploadPhoto();
+  const user = createUser();
+
+  return Promise.all([photo, user]).then((res) => {
+    console.log(`${res[0].body} ${res[1].firstName} ${res[1].lastName}`);
+  })
+    .catch(() => { console.log('Signup system offline'); });
 }
